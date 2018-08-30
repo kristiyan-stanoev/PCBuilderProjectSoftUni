65ef1d639d563af566fcedee4dc3cfc9b82a826a
kristiyan-stanoev <42832256+kristiyan-stanoev@users.noreply.github.com>
2018-08-30 12:17:17 +0300

    Add files via upload

---
+++ PCBuilder.Web/Pages/GraphicsCards/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PCBuilder.Data;
+using PCBuilder.Models;
+
+namespace PCBuilder.Web.Pages.GraphicsCards
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly PCBuilder.Data.PCBuilderContext _context;
+
+        public DetailsModel(PCBuilder.Data.PCBuilderContext context)
+        {
+            _context = context;
+        }
+
+        public GraphicsCard GraphicsCard { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            bool isInRole = this.User.IsInRole("Administrator");
+
+            GraphicsCard = await _context.GraphicsCards.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (GraphicsCard == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ PCBuilder.Web/Pages/GraphicsCards/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PCBuilder.Data;
+using PCBuilder.Models;
+
+namespace PCBuilder.Web.Pages.GraphicsCards
+{
+    public class IndexModel : PageModel
+    {
+        private readonly PCBuilder.Data.PCBuilderContext _context;
+
+        public IndexModel(PCBuilder.Data.PCBuilderContext context)
+        {
+            _context = context;
+        }
+
+        public IList<GraphicsCard> GraphicsCard { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            GraphicsCard = await _context.GraphicsCards.ToListAsync();
+        }
+    }
+}
