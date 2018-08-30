f58928cbd645857510ca59f304726d31375b4b06
kristiyan-stanoev <42832256+kristiyan-stanoev@users.noreply.github.com>
2018-08-30 12:12:13 +0300

    Add files via upload

---
+++ PCBuilder.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PCBuilder.Data;
+using PCBuilder.Models;
+
+[assembly: HostingStartup(typeof(PCBuilder.Web.Areas.Identity.IdentityHostingStartup))]
+namespace PCBuilder.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
